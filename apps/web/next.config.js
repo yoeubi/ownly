29263f1486d0f1ddd29de9b8f39cbc4e117f5251
yoeubi <yoeubi28@gmail.com>
2026-03-08 19:22:41 +0900

    Initial commit from create-turbo

---
+++ apps/web/next.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  reactStrictMode: true,
+  turbopack: {
+    resolveAlias: {
+      "react-native": "react-native-web",
+    },
+    resolveExtensions: [
+      ".web.js",
+      ".web.jsx",
+      ".web.ts",
+      ".web.tsx",
+      ".js",
+      ".jsx",
+      ".ts",
+      ".tsx",
+      ".json",
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Transform all direct `react-native` imports to `react-native-web`
+      "react-native$": "react-native-web",
+    };
+    config.resolve.extensions = [
+      ".web.js",
+      ".web.jsx",
+      ".web.ts",
+      ".web.tsx",
+      ...config.resolve.extensions,
+    ];
+    return config;
+  },
+};
---
+++ packages/ui/tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, Options } from "tsup";
+
+export default defineConfig((options: Options) => ({
+  entry: {
+    index: "src/index.tsx",
+  },
+  banner: {
+    js: "'use client'",
+  },
+  clean: true,
+  format: ["cjs", "esm"],
+  external: ["react"],
+  dts: {
+    compilerOptions: {
+      skipLibCheck: true,
+      types: [],
+    },
+  },
+  ...options,
+}));
